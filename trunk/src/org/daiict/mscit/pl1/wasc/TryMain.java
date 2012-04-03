3df07a8421905e286c12c8fc66da447620f08882
varunjamdar <varunjamdar@f5eef350-dda6-7a61-d075-06fc7e01c91b>
2012-04-03 08:24:32 +0000

    crawler under progress. It is able to crawl sub urls on its own but there is some problem that needs to be fixed.

---
+++ trunk/src/org/daiict/mscit/pl1/wasc/TryMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.daiict.mscit.pl1.WASC;
+/**
+ *
+ * @author VJ
+ */
+
+import org.daiict.mscit.pl1.wasc.db.*;
+import org.daiict.mscit.pl1.wasc.entities.*;
+import org.daiict.mscit.pl1.wasc.crawler.*;
+import java.sql.*;
+
+public class TryMain {
+
+    public static void main(String[] args) {
+        
+        Crawler crawler=new Crawler();
+        crawler.setBaseURL("http://www.jaydesai.co.in");
+        crawler.crawl();
+        
+    }
+}
--- trunk/src/org/daiict/mscit/pl1/wasc/db/DAL.java
+++ trunk/src/org/daiict/mscit/pl1/wasc/db/DAL.java
@@ -14,6 +14,7 @@ import org.daiict.mscit.pl1.wasc.entities.*;
 public class DAL {
 
     private DBConnection connection;
+    private PreparedStatement ps;
     private ResultSet resultset;
     private String database = "WASC";
 
@@ -25,20 +26,23 @@ public class DAL {
         return connection.connect(database);
     }
 
-    int getNextURLID() {
+    public int getNextURLID() {
         int id = -1;
         try {
             if (connect()) {
                 resultset = connection.customQuery("Select max(ID) from links");
                 if (resultset.next()) {
                     id = (resultset.getInt(1) == 0 ? 1 : resultset.getInt(1));
+                    id++;
                 } else {
                     id = 1;
                 }
                 connection.disconnect();
             }
         } catch (Exception ex) {
-            System.out.println("Connection Error occurred : " + ex.getMessage());
+            System.out.println("getNext URL - Connection Error occurred : " + ex.getMessage());
+        }finally{
+            connection.disconnect();
         }
         return id;
     }
@@ -46,14 +50,25 @@ public class DAL {
     public boolean persistLink(Link l) {
         try {
             if (connect()) {
-                boolean result= connection.executeQuery("INSERT INTO links (ID,URL,ParentURL,Done) VALUES(" + l.getID() + ",'" + l.getURL() + "','" + l.getParentURL() + "'," + l.getDone() + ")");
+                //need to implement using prepared statement
+                ps=connection.getConnection().prepareStatement("INSERT INTO links (ID,URL,ParentURL,Done) VALUES(?,?,?,?)");
+                ps.setInt(1, l.getID());
+                ps.setString(2, l.getURL());
+                ps.setString(3, l.getParentURL());
+                ps.setInt(4, l.getDone());
+                
+                ps.executeUpdate();
+                
+                //boolean result= connection.executeQuery("INSERT INTO links (ID,URL,ParentURL,Done) VALUES(" + l.getID() + ",'" + l.getURL() + "','" + l.getParentURL() + "'," + l.getDone() + ")");
                 connection.disconnect();
-                return result;
+                //return result;
             } else {
                 return false;
             }
         } catch (Exception ex) {
-            System.out.println("Connection Error occurred : " + ex.getMessage());
+            System.out.println("persist Link "+l.getURL()+" - Connection Error occurred : " + ex.getMessage());
+        }finally{
+            connection.disconnect();
         }
         return false;
     }
@@ -68,8 +83,28 @@ public class DAL {
                 return false;
             }
         } catch (Exception ex) {
-            System.out.println("Connection Error occurred : " + ex.getMessage());
+            System.out.println("Update Link "+l.getURL()+" - Connection Error occurred : " + ex.getMessage());
+        }finally{
+            connection.disconnect();
         }
         return false;
+    }
+    
+    public Link getNextUndoneLink(){
+        try{
+            if(connect()){
+                resultset=connection.customQuery("Select * from links where Done=1 Limit 1");
+                if(resultset.first()){
+                    Link l=new Link(resultset.getInt(1), resultset.getString(2), resultset.getString(3), resultset.getInt(5));
+                    connection.disconnect();
+                    return l;
+                }
+            }
+        }catch(Exception ex){
+            System.out.println("getNextUndoneLink - Connection Error occurred : " + ex.getMessage());
+        }finally{
+            connection.disconnect();
+        }
+        return null;
     }
 }
