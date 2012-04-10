17ece6ba882ebbd8f6de0dc9449fef7fe22969f8
shah.shikha.12 <shah.shikha.12@f5eef350-dda6-7a61-d075-06fc7e01c91b>
2012-04-10 10:47:37 +0000

    crawler done..

--- src/org/daiict/mscit/pl1/wasc/TryMain.java
+++ src/org/daiict/mscit/pl1/wasc/TryMain.java
@@ -8,17 +8,14 @@ package org.daiict.mscit.pl1.WASC;
  * @author VJ
  */
 
-import org.daiict.mscit.pl1.wasc.db.*;
-import org.daiict.mscit.pl1.wasc.entities.*;
 import org.daiict.mscit.pl1.wasc.crawler.*;
-import java.sql.*;
 
 public class TryMain {
 
     public static void main(String[] args) {
         
         Crawler crawler=new Crawler();
-        crawler.setBaseURL("http://www.twitter.com");
+        crawler.setBaseURL("http://placement.daiict.ac.in");
         crawler.crawl();
     }
 }
