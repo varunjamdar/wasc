f45ded8a7087b1184d2a593eb61a29609c7388fa
shah.shikha.12 <shah.shikha.12@f5eef350-dda6-7a61-d075-06fc7e01c91b>
2012-04-16 21:38:46 +0000

    update till 17th april

---
+++ src/org/daiict/mscit/pl1/wasc/security/SQLInjector.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.daiict.mscit.pl1.wasc.security;
+
+/**
+ *
+ * @author VJ
+ */
+public class SQLInjector {
+    
+    //how wud you pass the links to the sqlinjector to start doing its procedure?
+    //central link store???
+    //method that checks whether the link has a form or not.
+    //a function to check what kind of form it is, i.e. login form or register kind of.
+    //if yes then a nother method will do (various) sql injection(/s) on it.
+    //if success then it will further crawl to new discovered pages behing the sql injected response page
+    //follow the same procedure
+    
+    //public static boolean 
+    
+    public SQLInjector(){
+    }
+}
